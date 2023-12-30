6ce32e9616fe113ae46b6e497252b3e949b55616
benycarlitosganizanes <119455358+benycarlitosganizanes@users.noreply.github.com>
2023-12-30 08:31:51 +0200

    Create Bene.py

---
+++ Bene.py
@@ -0,0 +1,257 @@
+
+
+dicionario_programacao = {
+    
+    'package': 'pacote',
+    'argument': 'argumento',
+    'parameter': 'parâmetro',
+    'class': 'classe',
+    'object': 'objeto',
+    'method': 'método',
+    'inheritance': 'herança',
+    'interface': 'interface',
+    'exception': 'exceção',
+    'error': 'erro',
+    'attribute': 'atributo',
+    'pointer': 'ponteiro',
+    'reference': 'referência',
+    'operator': 'operador',
+    'statement': 'declaração',
+    'expression': 'expressão',
+    'conditional': 'condicional',
+    'iteration': 'iteração',
+    'algorithm': 'algoritmo',
+    'binary': 'binário',
+    'concatenation': 'concatenação', 
+    'assignment': 'atribuição',
+    'scope': 'escopo',
+    'syntax': 'sintaxe',
+    'comment': 'comentário',
+    'framework': 'framework',
+    'library': 'biblioteca',
+    'debugging': 'depuração',
+    'compile': 'compilar',
+    'interpreter': 'interpretador',
+    'recursion': 'recursão',
+    'encapsulation': 'encapsulamento',
+    'polymorphism': 'polimorfismo',
+    'abstraction': 'abstração',
+    'namespace': 'namespace',
+    'protocol': 'protocolo',
+    'callback': 'callback',
+    'package manager': 'gerenciador de pacotes',
+    'dependency': 'dependência',
+    'backend': 'backend ou parte de trás (como referência ao servidor)',
+    'frontend': 'frontend ou parte da frente (referente à interface do usuário)',
+    'endpoint': 'endpoint',
+    'middleware': 'middleware',
+    'prototype': 'protótipo',
+    'version control': 'controle de versão',
+    'merge': 'mesclar',
+    'branch': 'ramificação',
+    'commit': 'commit ou confirmação',
+    'pull request': 'solicitação de pull',
+    'code review': 'revisão de código',
+    'agile': 'ágil',
+    'scrum': 'scrum',
+    'sprint': 'sprint',
+    'user story': 'história do usuário',
+    'product owner': 'dono do produto',
+    'scrum master': 'scrum master',
+    'continuous integration': 'integração contínua',
+    'continuous deployment': 'implantação contínua',
+    'devops': 'devops',
+    'container': 'contêiner',
+    'virtual machine': 'máquina virtual',
+    'api': 'api ou interface de programação de aplicações',
+    'sdk': 'kit de desenvolvimento de software (software development kit)',
+    'data structure': 'estrutura de dados',
+    'linked list': 'lista encadeada',
+    'stack': 'pilha',
+    'queue': 'fila',
+    'tree': 'árvore',
+    'graph': 'grafo',
+    'hash table': 'tabela hash',
+    'binary search': 'busca binária',
+    'depth-first search (dfs)': 'busca em profundidade',
+    'breadth-first search (bfs)': 'busca em largura',
+    'dynamic programming': 'programação dinâmica',
+    'greedy algorithm': 'algoritmo guloso',
+    'big o notation': 'notação big o',
+    'asymptotic analysis': 'análise assintótica',
+    'backtracking': 'retrocesso',
+    'dijkstra\'s algorithm': 'algoritmo de dijkstra',
+    'bellman-ford algorithm': 'algoritmo de bellman-ford',
+    'floyd-warshall algorithm': 'algoritmo de floyd-warshall',
+    'kruskal\'s algorithm': 'algoritmo de kruskal',
+    'prim\'s algorithm': 'algoritmo de prim',
+    'topological sort': 'ordenção topológica',
+    'heap': 'heap ou montículo',
+    'priority queue': 'fila de prioridade',
+    'trie': 'trie ou árvore digital',
+    'red-black tree': 'árvore rubro-negra',
+    'avl tree': 'árvore avl',
+    'segment tree': 'árvore de segmentos',
+    'binary indexed tree (bit)': 'árvore binária indexada',
+    'fenwick tree': 'árvore de fenwick',
+    'suffix array': 'arranjo de sufixos',
+    'suffix tree': 'árvore de sufixos',
+    'bloom filter': 'filtro de bloom',
+    'spanning tree': 'árvore geradora',
+    'minimum spanning tree (mst)': 'árvore geradora mínima', 'transversal': 'travessia',
+    'serialization': 'serialização',
+    'deserialization': 'desserialização',
+    'concurrency': 'concorrência',
+    'parallelism': 'paralelismo',
+    'thread': 'thread ou rosca',
+    'mutex': 'mutex',
+    'semaphore': 'semáforo',
+    'deadlock': 'deadlock ou impasse',
+    'race condition': 'condição de corrida',
+    'critical section': 'seção crítica',
+    'synchronization': 'sincronização',
+    'cache': 'cache ou cachê',
+    'ram': 'ram ou memória principal',
+    'rom': 'rom ou memória de somente leitura',
+    'cpu': 'cpu ou unidade central de processamento',
+    'gpu': 'gpu ou unidade de processamento gráfico',
+    'disk': 'disco',
+    'ssd': 'ssd ou unidade de estado sólido',
+    'hdd': 'hdd ou disco rígido',
+    'file system': 'sistema de arquivos',
+    'binary file': 'arquivo binário',
+    'text file': 'arquivo de texto',
+    'directory': 'diretório',
+    'path': 'caminho',
+    'root': 'raiz',
+    'file descriptor': 'descritor de arquivo',
+    'i/o': 'e/s ou entrada/saída',
+    'file i/o': 'e/s de arquivo',
+    'network': 'rede',
+    'tcp/ip': 'tcp/ip',
+    'http': 'http',
+    'https': 'https',
+    'ftp': 'ftp ou protocolo de transferência de arquivos',
+    'api': 'api ou interface de programação de aplicações',
+    'restful': 'restful',
+    'json': 'json ou notação de objeto javascript',
+    'xml': 'xml ou linguagem de marcação extensível',
+    'soap': 'soap ou protocolo de acesso a objeto simples',
+    'websocket': 'websocket',
+    'dns': 'dns ou sistema de nomes de domínio',
+    'url': 'url ou localizador uniforme de recursos',
+    'uri': 'uri ou identificador de recursos uniforme',
+    'ssl/tls': 'ssl/tls',
+    'ssh': 'ssh ou shell seguro',
+    'firewall': 'firewall',
+    'proxy': 'proxy',
+    'load balancing': 'balanceamento de carga',
+    'packet': 'pacote',
+    'routing': 'roteamento',
+    'ipv4': 'ipv4',
+    'ipv6': 'ipv6',
+    'lan': 'lan ou rede local',
+    'wan': 'wan ou rede de área ampla',
+    'vpn': 'vpn ou rede privada virtual',
+    'ethernet': 'ethernet',
+    'wireless': 'sem fio',
+    'bluetooth': 'bluetooth',
+    'iot': 'iot ou internet das coisas',
+    'sensor': 'sensor',
+    'actuator': 'atuador',
+    'microcontroller': 'microcontrolador',
+    'microprocessor': 'microprocessador',
+    'compiler': 'compilador',
+    'interpreter': 'interpretador',
+    'source code': 'código fonte',
+    'executable': 'executável',
+    'binary code': 'código binário',
+    'bytecode': 'bytecode ou código de byte',
+    'assembler': 'assembler ou montador',
+    'machine code': 'código de máquina',
+    'ide': 'ide ou ambiente de desenvolvimento integrado',
+    'text editor': 'editor de texto',
+    'debugger': 'depurador',
+    'profiler': 'profiler ou perfilador',
+    'refactoring': 'refatoração',
+    'variable declaration': 'declaração de variável',
+    'data type': 'tipo de dados',
+    'assignment operator': 'operador de atribuição',
+    'arithmetic operator': 'operador aritmético',
+    'logical operator': 'operador lógico',
+    'comparison operator': 'operador de comparação',
+    'increment operator': 'operador de incremento',
+    'decrement operator': 'operador de decremento',
+    'ternary operator': 'operador ternário',
+    'conditional statement': 'instrução condicional',
+    'if statement': 'instrução if',
+    'else statement': 'instrução else',
+    'else if statement': "instruções else + if que na lógica vai resultar um if "
+
+
+
+
+}
+
+def sobre_me():
+    print("————————————————————")
+    print("📚Olá, meu nome é Beny, tenho 20 anos e atualmente estou fazendo mestrado em ensino de inglês para ESL na Universidade Licungo de Moçambique. Sou um estudante apaixonado por tecnologia, principalmente quando se trata de programar em Python 3. Estou aprendendo aos poucos, contando com a ajuda de bons amigos online e grupos. Acredito que em breve poderei me considerar um programador. Confira meu canal no YouTube, onde falo sobre vários temas de tecnologia, desde o básico até o avançado, do específico ao geral:🔰🔰")
+    print("____________________________________________________________") 
+    print("🔛Canal YouTube: https://youtube.com/@benysartandroidmoz1141?sub_confirmation=1")
+    print("————————————————————————————————————————————————————\n")
+    print('WhatsApp:\n')
+    print('📌📌+258863679623🔛')
+    print("————————————————————————————————————————————————————\n")
+
+def pesquisar_termo(nome):
+    if nome in dicionario_programacao:
+        return dicionario_programacao[nome]
+    else:
+        return "Desculpe, o termo não está disponível ainda."
+
+def listar_por_letra(letra):
+    termos_encontrados = [termo for termo in dicionario_programacao.keys() if termo.startswith(letra.lower() or letra.upper())]
+    if termos_encontrados:
+        return termos_encontrados
+    else:
+        return "Nenhum termo encontrado para essa letra. Por favor, reporte ao programador."
+
+def main():
+    while True:
+        print("🔰SEJAM BEM-VINDOS DEVS 🔰")
+        print("Programa dedicado a vocês!!!\n ")
+        print("1. Pesquisar por nome")
+        print("2. Procurar por letra inicial")
+        print("3. Sobre o Programa")
+        print("4. Sobre o Programador")
+        print("5. Sair\n")
+        print('_____////____////_____////____////____////___////____////,\n')
+
+        opcao = input("🔰Digite o número da opção desejada: ")
+
+        if opcao == '1':
+            termo_pesquisado = input("Digite o termo que deseja pesquisar:\n ")
+            resultado_pesquisa = pesquisar_termo(termo_pesquisado.lower())
+            print(resultado_pesquisa)
+            continuar = input("Deseja continuar? (S/N): ")
+            if continuar.upper() != 'S':
+                break
+        elif opcao == '2':
+            letra_inicial = input("Digite a letra inicial para buscar termos:\n ")
+            termos_encontrados = listar_por_letra(letra_inicial.lower())
+            print(termos_encontrados)
+            continuar = input("Deseja continuar? (S/N): ")
+            if continuar.upper() != 'S':
+                break
+        elif opcao == '3':
+            sobre_me()
+        elif opcao == '4':
+            sobre_me()
+        elif opcao == '5':
+            print("Saindo do programa...")
+            break
+        else:
+            print("Opção inválida. Tente novamente.")
+
+if __name__ == "__main__":
+    main()
